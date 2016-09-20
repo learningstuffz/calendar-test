ff2726295e213baea75f14b4e182f3219799c5ea
debu <rony.debu@gmail.com>
2016-09-20 22:33:31 +0530

    Beautify code

--- src/custom/js/main.js
+++ src/custom/js/main.js
@@ -1,126 +1,123 @@
- $(document).ready(function () {
+$(document).ready(function() {
 
 
-            date = getDate();
-            var eventsData = [];
-            if (typeof (Storage) !== "undefined") {
-                eventsData = JSON.parse(localStorage.getItem("details"));
-                if (eventsData === null) {
-                    eventsData = [];
-                }
-                else if (eventsData.length === undefined) {
-                    eventsData = [eventsData];
-                }
-            }
-            else {
-                //sorry storage not supported
-            }
-            $('#calendar').draggable();
-            $('#calendar').fullCalendar({
-                header: {
-                    left: 'prev,next',
-                    center: 'title',
-                    right: 'month'
-                },
-                defaultDate: date,
-                editable: false,
-                eventLimit: true, // allow "more" link when too many events
-                events: eventsData
-            });
-            $('#txtDates').datepicker({ autoSize: true });
-            initialize();
-        });
-var calendarRefresh=function(){
+    date = getDate();
     var eventsData = [];
-            if (typeof (Storage) !== "undefined") {
-                eventsData = JSON.parse(localStorage.getItem("details"));
-                if (eventsData === null) {
-                    eventsData = [];
-                }
-                else if (eventsData.length === undefined) {
-                    eventsData = [eventsData];
-                }
-            }
-            else {
-                //sorry storage not supported
-            }
-            $('#calendar').fullCalendar( 'removeEvents');
-            $('#calendar').fullCalendar( 'addEventSource', eventsData);
-};
-        function callingAjax(t, u, s, e) {
-            $.ajax({
-                type: t,
-                url: u,
-                success: s,
-                error: e
-            });
+    if (typeof(Storage) !== "undefined") {
+        eventsData = JSON.parse(localStorage.getItem("details"));
+        if (eventsData === null) {
+            eventsData = [];
+        } else if (eventsData.length === undefined) {
+            eventsData = [eventsData];
         }
-        var initialize = function () {
-            $('#btnAdd').click(function () {
-                if (typeof (Storage) !== "undefined") {
+    } else {
+        //sorry storage not supported
+    }
+    $('#calendar').draggable();
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next',
+            center: 'title',
+            right: 'month'
+        },
+        defaultDate: date,
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        events: eventsData
+    });
+    $('#txtDates').datepicker({
+        autoSize: true
+    });
+    initialize();
+});
+var calendarRefresh = function() {
+    var eventsData = [];
+    if (typeof(Storage) !== "undefined") {
+        eventsData = JSON.parse(localStorage.getItem("details"));
+        if (eventsData === null) {
+            eventsData = [];
+        } else if (eventsData.length === undefined) {
+            eventsData = [eventsData];
+        }
+    } else {
+        //sorry storage not supported
+    }
+    $('#calendar').fullCalendar('removeEvents');
+    $('#calendar').fullCalendar('addEventSource', eventsData);
+};
 
-                    // Code for localStorage/sessionStorage.
-                    var dat = $('#txtDates').val();
-                    dat = new Date(dat);
-                                        if (dat == "Invalid Date") {
-                                            $('#txtDates').val("Select date");
-                                            return false;
-                                        }
-                    var obj = JSON.parse(localStorage.getItem("details"));
+function callingAjax(t, u, s, e) {
+    $.ajax({
+        type: t,
+        url: u,
+        success: s,
+        error: e
+    });
+}
+var initialize = function() {
+    $('#btnAdd').click(function() {
+        if (typeof(Storage) !== "undefined") {
 
-                    if (obj !== null) {
-                        console.log(obj);
-                        if (obj.length !== undefined) {
-                            obj.push({
-                                title: $('#txtEvents').val(),
-                                start: getDate($('#txtDates').val()),
-                                backgroundColor: "red"
-                            });
-                            localStorage.setItem("details", JSON.stringify(obj));
-                        }
-                        else {
-                            var ind = [obj, {
-                                title: $('#txtEvents').val(),
-                                start: getDate($('#txtDates').val()),
-                                backgroundColor: "red"
-                            }];
-                            localStorage.setItem("details", JSON.stringify(ind));
-                        }
-                    }
-                    else {
-                        obj = {
-                            title: $('#txtEvents').val(),
-                            start: getDate($('#txtDates').val()),
-                            backgroundColor: "red"
-                        };
-                        localStorage.setItem("details", JSON.stringify(obj));
-                    }
-                    calendarRefresh();
+            // Code for localStorage/sessionStorage.
+            var dat = $('#txtDates').val();
+            dat = new Date(dat);
+            if (dat == "Invalid Date") {
+                $('#txtDates').val("Select date");
+                return false;
+            }
+            var obj = JSON.parse(localStorage.getItem("details"));
 
+            if (obj !== null) {
+                console.log(obj);
+                if (obj.length !== undefined) {
+                    obj.push({
+                        title: $('#txtEvents').val(),
+                        start: getDate($('#txtDates').val()),
+                        backgroundColor: "red"
+                    });
+                    localStorage.setItem("details", JSON.stringify(obj));
                 } else {
-                    alert('Sorry it seems there is no local storage supported in your browser!!');
-
+                    var ind = [obj, {
+                        title: $('#txtEvents').val(),
+                        start: getDate($('#txtDates').val()),
+                        backgroundColor: "red"
+                    }];
+                    localStorage.setItem("details", JSON.stringify(ind));
                 }
+            } else {
+                obj = {
+                    title: $('#txtEvents').val(),
+                    start: getDate($('#txtDates').val()),
+                    backgroundColor: "red"
+                };
+                localStorage.setItem("details", JSON.stringify(obj));
+            }
+            calendarRefresh();
 
-            });
-        };
-        var getDate = function (dt) {
+        } else {
+            alert('Sorry it seems there is no local storage supported in your browser!!');
 
-            var d = new Date();
-            if (dt !== undefined) {
-                d = new Date(dt);
-            }
-            var yr = d.getFullYear();
-            var mn = d.getMonth() + 1;
-            var day = d.getDate();
-            var date = yr + "-";
-            if (mn < 10) {
-                date += "0";
-            }
-            date += mn + "-";
-            if (day < 10) {
-                date += "0";
-            }
-            date += day;
-            return date;
-        };
+        }
+
+    });
+};
+var getDate = function(dt) {
+
+    var d = new Date();
+    if (dt !== undefined) {
+        d = new Date(dt);
+    }
+    var yr = d.getFullYear();
+    var mn = d.getMonth() + 1;
+    var day = d.getDate();
+    var date = yr + "-";
+    if (mn < 10) {
+        date += "0";
+    }
+    date += mn + "-";
+    if (day < 10) {
+        date += "0";
+    }
+    date += day;
+    return date;
+};
